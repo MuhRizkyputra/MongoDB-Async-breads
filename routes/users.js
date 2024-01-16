f723f7f924354ba02418052b6365a22ff7ebda94
Muhammad <muhammadrizkyputraaa@gmail.com>
2024-01-16 22:31:55 +0700

    Router Users Done

--- routes/users.js
+++ routes/users.js
@@ -3,54 +3,66 @@ const { ObjectId } = require('mongodb');
 var router = express.Router();
 
 
-module.exports = function (db){
-const User = db.collection('users')
+module.exports = function (db) {
+  const User = db.collection('users')
 
 
-router.get('/', async function (req, res , next){
- 
-  try {
-    const data = await User.find().toArray()
-    console.log('test', data)
-  res.status(200).json({user : data});
-  } catch {
-    res.status(500).json(err)
-  }
-})
+  router.get('/', async function (req, res, next) {
+
+    try {
+      const data = await User.find().toArray()
+      console.log('test', data)
+      res.status(200).json({ data })
+    } catch {
+      res.status(500).json(err)
+    }
+  })
 
 
-router.get('/:id', async function (req, res) {
-  try {
-    const id = req.params.id
-    const user = await User.findOne({_id: new ObjectId(id) })
-    res.status(201).json(user)
+  router.get('/:id', async function (req, res) {
+    try {
+      const id = req.params.id
+      const user = await User.findOne({ _id: new ObjectId(id) })
+      res.status(201).json(user)
 
-  }catch (error) {
-    res.status(500).json(error)
-  }
-})
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  })
 
-router.post('/', async function (req, res) {
-  try {
-    const {name , phone} = req.body
-    const user = await User.insertOne({ name , phone })
-    const data = await User.findOne({ _id : new ObjectId(user.insertedId)})
-    res.status(200).json(data)
+  router.post('/', async function (req, res) {
+    try {
+      const { name, phone } = req.body
+      const user = await User.insertOne({ name, phone })
+      const data = await User.findOne({ _id: new ObjectId(user.insertedId) })
+      res.status(200).json(data)
 
-  }catch (error) {
-    res.status(500).json({ error: error.message })
-  }
-})
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  })
 
-router.delete('/:id', async function (req, res) {
-  try {
-    const id = req.params.id
-    const user = await User.findOneAndDelete({ _id: new ObjectId(id)})
-  res.json(user)
+  router.put('/:id', async function (req, res) {
+    try {
+      const { name, phone } = req.body
+      const id = req.params.id
+      const updatedUser = await User.findOneAndUpdate({ _id: new ObjectId(id) }, { $set: { name: name, phone: phone } })
+      const user = await User.findOne({ _id: new ObjectId(id) })
+      res.status(201).json(user)
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  })
 
-  }catch (error) {
-    res.status(500).json({ error: error.message })
-  }
-})
-return router
+  router.delete('/:id', async function (req, res) {
+    try {
+      const id = req.params.id
+      const user = await User.findOneAndDelete({ _id: new ObjectId(id) })
+      res.json(user)
+
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  })
+  return router
 }
