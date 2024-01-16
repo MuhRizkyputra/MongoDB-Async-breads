08c76399b49b9129965ff5a0622520632d1adb6f
Muhammad <muhammadrizkyputraaa@gmail.com>
2024-01-16 14:00:09 +0700

    Backend in progress

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
+const { ObjectId } = require('mongodb');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+module.exports = function (db){
+
+}
 
 module.exports = router;
